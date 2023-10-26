1e9fb287693b4515b10b5a1efa013922d79f626b
embry <106962320+embry0n@users.noreply.github.com>
2023-10-26 10:42:36 +0500

    Delete 8/main8_1v2.py

--- 8/main8_1v2.py
+++
@@ -1,7 +0,0 @@
-from rutube import Rutube
-
-rt = Rutube('https://rutube.ru/video/5c5f0ae2d9744d11a05b76bd327cbb51/')
-
-print(rt.playlist)  # [Nature 4k (272x480), Nature 4k (408x720), Nature 4k (608x1080)]
-
-rt.playlist[-1].download()
